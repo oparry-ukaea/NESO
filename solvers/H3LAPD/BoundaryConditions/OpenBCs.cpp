0ddffbe0f104edd00d09bd1dcc1609555f9255c2
Owen Parry <owen.parry@ukaea.uk>
2023-11-20 10:10:46 +0000

    Nudge BCs. Doesn't help.

--- solvers/H3LAPD/BoundaryConditions/OpenBCs.cpp
+++ solvers/H3LAPD/BoundaryConditions/OpenBCs.cpp
@@ -25,6 +25,44 @@ OpenBCs::OpenBCs(const LU::SessionReaderSharedPtr &pSession,
 void OpenBCs::v_Apply(Array<OneD, Array<OneD, NekDouble>> &Fwd,
                       Array<OneD, Array<OneD, NekDouble>> &physarray,
                       const NekDouble &time) {
-  boost::ignore_unused(time);
+  const Array<OneD, const int> &traceBndMap =
+      m_fields[m_dens_idx]->GetTraceBndMap();
+
+  // Loop over all explists in this region
+  auto explists = m_fields[m_dens_idx]->GetBndCondExpansions()[m_bcRegion];
+  // std::cout << "Setting region " << m_bcRegion << " BCs" << std::endl;
+  for (int e = 0; e < explists->GetExpSize(); ++e) {
+    // std::cout << "  Setting exp " << e << " vals" << std::endl;
+    //  Current explist
+    LR::ExpansionSharedPtr explist = explists->GetExp(e);
+    // Offset in the field arrays for this explist
+    int explist_offset = explists->GetPhys_Offset(e);
+    // Offset in the trace map for this explist
+    int trace_offset = m_fields[m_dens_idx]->GetTrace()->GetPhys_Offset(
+        traceBndMap[m_offset + e]);
+
+    Array<OneD, NekDouble> xc(explist->GetTotPoints());
+    explist->GetCoords(xc);
+
+    Array<OneD, NekDouble> target(explist->GetTotPoints());
+    evaluate_expression(explist, target);
+    // Loop over points in this explist
+    for (int idx_in_explist = 0; idx_in_explist < explist->GetTotPoints();
+         idx_in_explist++) {
+      int pt_idx = trace_offset + idx_in_explist;
+
+      // std::cout << "xcoord = " << xc[idx_in_explist] << std::endl;
+
+      NekDouble current_val = m_fields[m_dens_idx]->GetPhys()[pt_idx];
+      NekDouble target_val = target[idx_in_explist];
+      (m_fields[m_field_idx]
+           ->GetBndCondExpansions()[m_bcRegion]
+           ->UpdatePhys())[explist_offset + idx_in_explist] =
+          (0.8 * current_val + 0.2 * target_val);
+      // std::cout << "Setting " << 0.8 * current_val + 0.2 * target_val
+      //           << std::endl;
+      // int foo = 1;
+    }
+  }
 }
 } // namespace NESO::Solvers::H3LAPD
