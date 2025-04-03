8a58e2fd29cf5ee08703a2c8aeb648f44e20b837
Owen Parry <owen.parry@ukaea.uk>
2025-04-03 09:24:43 +0100

    intel-gdb wrapper

---
+++ gdb_wrapper.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. /home/oparry/software/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/intel-oneapi-compilers-2024.1.0-xuha7kdygfergjpa6t6xqllz3pzeiy4y/debugger/2024.1/env/vars.sh
+
+/home/oparry/software/spack/opt/spack/linux-ubuntu22.04-skylake/gcc-11.3.0/intel-oneapi-compilers-2024.1.0-xuha7kdygfergjpa6t6xqllz3pzeiy4y/debugger/2024.1/bin/gdb-oneapi $@
